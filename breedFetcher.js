c8ae192f1ff70a7c4c14fe955574d385edcfacb5
Tyree Webber <webber.tyree@gmail.com>
2022-06-09 18:41:27 -0600

    refactored code using callbacks

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,21 @@
 const request = require('request');
-const breedName = process.argv[2];
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
-  if (error) {
-    console.log('error: ', error);
-  }
-  const data = JSON.parse(body);
-  if (!data[0]) {
-    console.log(`Sorry! ${breedName} is not a cat breed.`);
-    return;
-  }
-  if (response.statusCode === 200) {
-    console.log(`description: ${data[0].description}`);
-  }
-});
+const fetchBreedDescription = (breedName, callback) => {
+  const url = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedName;
+  request(url, (error, response, body) => {
+    if (error) {
+      callback('error found: ', null);
+      return;
+    }
+    const breed = JSON.parse(body);
+    if (!breed[0]) {
+      callback(`Sorry! ${breedName} is not a cat breed.`, null);
+      return;
+    }
+    if (response.statusCode === 200) {
+      callback(null, breed[0].description);
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
